abb76cb0d0e7b490546053895b4fc6a24f78cd02
eversilva <everson.silva2706@gmail.com>
2022-10-08 15:34:11 -0300

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get 'welcome/index'
   resources :coins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'welcome#index'
 end
