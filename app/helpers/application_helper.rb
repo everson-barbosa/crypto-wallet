be55e460be8ad1d3909acd721876b2b630df5846
eversilva <everson.silva2706@gmail.com>
2022-10-13 22:45:16 -0300

    Add application_enviroment helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,14 @@ module ApplicationHelper
     def application_name
         'Crypto Wallet'
     end
+
+    def application_environment
+        if Rails.env.development?
+            'Desenvolvimento'
+        elsif Rails.env.production?
+            'Produção'
+        else
+            'Teste'
+        end
+    end
 end
