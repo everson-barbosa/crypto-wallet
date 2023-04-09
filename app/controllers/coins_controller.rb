e89fdd8f6121ed8e5f0706b12e815abbf26d42a5
EverSilva <everson.silva2706@gmail.com>
2023-04-09 18:15:06 -0300

    refactor: create method set_mining_type_options in coin controller

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,5 +1,6 @@
 class CoinsController < ApplicationController
   before_action :set_coin, only: %i[ show edit update destroy ]
+  before_action :set_mining_type_options, only: %i[ new create edit update ]
 
   # GET /coins or /coins.json
   def index
@@ -58,6 +59,10 @@ class CoinsController < ApplicationController
   end
 
   private
+    def set_mining_type_options
+      @mining_type_options = MiningType.all.pluck(:description, :id)
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_coin
       @coin = Coin.find(params[:id])
